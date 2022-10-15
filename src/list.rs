b35673efbcfb5b4ba70547654c37595631cd899d
Takashi Toyoshima <toyoshim@gmail.com>
2022-10-15 13:44:33 +0900

    Dump more descriptor information

--- src/list.rs
+++ src/list.rs
@@ -32,24 +32,17 @@ pub fn run(vid: Option<u16>, pid: Option<u16>) {
             continue;
         }
         println!(
-            "  Bus: {:>2}, Port: {:>2}, Address: {:>3}, Speed: {}",
+            "  Bus: {:>2}, Port: {:>2}, Address: {:>3}, Speed: {:?}",
             device.bus_number(),
             device.port_number(),
             device.address(),
-            match device.speed() {
-                rusb::Speed::Low => "Low",
-                rusb::Speed::Full => "Full",
-                rusb::Speed::High => "High",
-                rusb::Speed::Super => "Super",
-                rusb::Speed::SuperPlus => "Super+",
-                _ => "Unknown",
-            }
+            device.speed()
         );
-        println!("  Device Descriptor:");
-        println!("    USB Version: {}", device_desc.usb_version());
-        println!("    Device Version: {}", device_desc.device_version());
+        println!("    Device Descriptor:");
+        println!("      USB Version: {}", device_desc.usb_version());
+        println!("      Device Version: {}", device_desc.device_version());
         println!(
-            "    Vendor ID: {:04X} ({})",
+            "      Vendor ID: {:04X} ({})",
             device_desc.vendor_id(),
             match usb_ids::Vendor::from_id(device_desc.vendor_id()) {
                 Some(vendor) => vendor.name(),
@@ -57,38 +50,73 @@ pub fn run(vid: Option<u16>, pid: Option<u16>) {
             }
         );
         println!(
-            "    Product ID: {:04X} ({})",
+            "      Product ID: {:04X} ({})",
             device_desc.product_id(),
             match usb_ids::Device::from_vid_pid(device_desc.vendor_id(), device_desc.product_id()) {
                 Some(product) => product.name(),
                 None => "unknown",
             }
         );
-        println!("    Class: {}", device_desc.class_code());
-        println!("    Sub Class: {}", device_desc.sub_class_code());
-        println!("    Protocol: {}", device_desc.protocol_code());
-        println!("    Max Packet Size: {}", device_desc.max_packet_size());
+        println!("      Class: {}", device_desc.class_code());
+        println!("      Sub Class: {}", device_desc.sub_class_code());
+        println!("      Protocol: {}", device_desc.protocol_code());
+        println!("      Max Packet Size: {}", device_desc.max_packet_size());
         println!(
-            "    Num Configurations: {}",
+            "      Num Configurations: {}",
             device_desc.num_configurations()
         );
         if device_desc.manufacturer_string_index().is_some() {
             println!(
-                "    Manufacturer: {}",
+                "      Manufacturer: {}",
                 device_desc.manufacturer_string_index().unwrap()
             );
         }
         if device_desc.product_string_index().is_some() {
             println!(
-                "    Product: {}",
+                "      Product: {}",
                 device_desc.product_string_index().unwrap()
             );
         }
         if device_desc.serial_number_string_index().is_some() {
             println!(
-                "    Serial Number: {}",
+                "      Serial Number: {}",
                 device_desc.serial_number_string_index().unwrap()
             );
+        }
+        for config_index in 0..device_desc.num_configurations() {
+            println!("    Configulation {}:", config_index);
+            let config_desc = match device.config_descriptor(config_index) {
+                Ok(desc) => desc,
+                Err(e) => {
+                    println!("    Failed to read the config descriptor: {}", e);
+                    continue;
+                }
+            };
+            println!("      Max Power: {}", config_desc.max_power());
+            println!("      Self Powered: {}", config_desc.self_powered());
+            println!("      Remote Wakeup: {}", config_desc.remote_wakeup());
+            println!("      Num of Interfaces: {}", config_desc.num_interfaces());
+
+            for interface in config_desc.interfaces() {
+                println!("    Interface {}:", interface.number());
+                let desc = interface.descriptors().next().unwrap();
+                println!("      Alternative Setting: {}", desc.setting_number());
+                println!("      Class: {}", desc.class_code());
+                println!("      Sub Class: {}", desc.sub_class_code());
+                println!("      Protocol: {}", desc.protocol_code());
+                println!("      Num of Endpoints: {}", desc.num_endpoints());
+                for ep in desc.endpoint_descriptors() {
+                    println!("      Endpoint {} ({:02X}):", ep.number(), ep.address());
+                    println!("        Direction: {:?}", ep.direction());
+                    println!("        Transfer Type: {:?}", ep.transfer_type());
+                    println!("        Sync Type: {:?}", ep.sync_type());
+                    println!("        Usage Type: {:?}", ep.usage_type());
+                    println!("        Max Packet Size: {}", ep.max_packet_size());
+                    println!("        Interval: {}", ep.interval());
+                    println!("        Refresh: {}", ep.refresh());
+                    println!("        Synch Address: {}", ep.synch_address());
+                }
+            }
         }
     }
 }
